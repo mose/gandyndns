20e1af0840a2aa41473f836f53a983c98a069c1c
mose <mose@mose.com>
2020-05-02 18:30:35 +0800

    add possibility to use several subdomains

--- README.md
+++ README.md
@@ -4,14 +4,14 @@ Gandi LiveDNS beta script
 Prequisites
 -----------------
 - a server with php
-- a gandi beta account on http://beta.gandi.net
+- a gandi account on https://gandi.net
 - a domain name using `ns1.gandi.net` as nameserver
 - a zone associated to that domain (follow instructions on http://doc.livedns.gandi.net/)
 
 Howto
 ---------
 - place my-ip.php on some public server
-- copy config.dist to config and modify values
+- copy config.dist to config and modify values (you can add several subdomains separated by a space)
 - cron the bash script, for example
   `*/10 * * * * cd $HOME/projects/gandyndns && ./refresh_IP.sh`
 
--- refresh_IP.sh
+++ refresh_IP.sh
@@ -7,18 +7,20 @@ CURRENT_IP=`cat ./current_ip`
 NEW_IP=`curl -s $GETIP_URL`
 
 d=`date`
-
 $DEBUG && echo "$d - Current IP: $CURRENT_IP" >> $LOGFILE
 $DEBUG && echo "$d - New IP:     $NEW_IP"     >> $LOGFILE
 
 if [ "$CURRENT_IP" != "$NEW_IP" ]
 then
   echo -n "$d - IP changed $CURRENT_IP -> $NEW_IP / " >> $LOGFILE
-  curl -s -X PUT -H "Content-Type: application/json" \
-    -H "X-Api-Key: $API_KEY" \
-    -d "{\"rrset_ttl\": 1800, \"rrset_values\": [\"$NEW_IP\"]}" \
-    https://dns.api.gandi.net/api/v5/zones/$ZONE/records/$SUBDOMAIN/A \
-    >> $LOGFILE
+  for domain in $SUBDOMAIN
+  do
+    curl -s -X PUT -H "Content-Type: application/json" \
+      -H "X-Api-Key: $API_KEY" \
+      -d "{\"rrset_ttl\": 1800, \"rrset_values\": [\"$NEW_IP\"]}" \
+      https://dns.api.gandi.net/api/v5/zones/$ZONE/records/$domain/A \
+      >> $LOGFILE
+  done
   echo >> $LOGFILE
   echo $NEW_IP > ./current_ip
 else
@@ -27,3 +29,4 @@ fi
 
 $DEBUG && echo "$d - Done" >> $LOGFILE
 exit 0
+
